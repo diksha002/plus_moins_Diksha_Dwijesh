2cc19e5bd2586dd09973e6bbcc10524162ed165b
diksha002 <diksha.raghoonundun@gmail.com>
2020-04-09 20:22:57 +0400

    Mode de difficulté

--- script/plus_moins.c
+++ script/plus_moins.c
@@ -23,14 +23,57 @@ int main()
 	srand (time(NULL));
 	int Valeur_a_trouver = rand() % 101;
 	int choix_joueur;
-	printf("inserez une valeur: ");
-	scanf("%d",&choix_joueur);
-	plus_moins(choix_joueur,Valeur_a_trouver);
-	while(choix_joueur != Valeur_a_trouver)
+	printf("Choisissez un niveau entre:facile, moyenne ou difficile \n");
+	printf("Inserez 1 pour facile, 2 pour moyenne et 3 pour difficile: \n");
+	int a;
+	scanf("%d",&a);
+	int count=0;
+	int count1=0;
+	switch(a)
 	{
-		printf("inserez une valeur: ");
-		scanf("%d",&choix_joueur);
-		plus_moins(choix_joueur,Valeur_a_trouver);
+		case 1:
+			do
+			{
+				printf("inserez une valeur: ");
+				scanf("%d",&choix_joueur);
+				plus_moins(choix_joueur,Valeur_a_trouver);
+			}
+			while(choix_joueur != Valeur_a_trouver);
+			break;
+		case 2:
+			do
+			{
+				printf("inserez une valeur: ");
+				scanf("%d",&choix_joueur);
+				count++;
+				plus_moins(choix_joueur,Valeur_a_trouver);
+				if(count==25)
+				{
+					printf("Vous avez dépassé la limite d'essaie\n");
+					break;
+				}
+			}
+			while(choix_joueur != Valeur_a_trouver);
+			break;
+		case 3:
+			do
+			{
+				printf("inserez une valeur: ");
+				scanf("%d",&choix_joueur);
+				count1++;
+				plus_moins(choix_joueur,Valeur_a_trouver);
+				if(count1==10)
+				{
+					printf("Vous avez dépassé la limite d'essaie\n");
+					break;
+				}
+			}
+			while(choix_joueur != Valeur_a_trouver);
+			break;
+		default:
+			printf("vous deviez choisir entre les 3 options disponible\n");
+			break;
 	}
+
 
 }
